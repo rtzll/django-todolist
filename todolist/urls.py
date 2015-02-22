260fbdab73b136c4b4b9c19c144a6af966aa2b9f
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-22 20:42:00 +0100

    Changed urls to include lists.

--- todolist/urls.py
+++ todolist/urls.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todolist.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', include('lists.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
