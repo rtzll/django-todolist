6188a88cea3898cd4c67f3047db07deb31ea931a
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-23 23:01:23 +0100

    Added attributes to Todo model.

--- lists/models.py
+++ lists/models.py
@@ -14,6 +14,9 @@ class TodoList(models.Model):
 class Todo(models.Model):
     description = models.CharField(max_length=128)
     created_at = models.DateTimeField(auto_now=True)
+    finished_at = models.DateTimeField()
+    is_finished = models.BooleanField(default=False)
+    creator = models.ForeignKey(User)
     todolist = models.ForeignKey(TodoList)
 
     def __str__(self):
