d6792853ac23a67f07487aa3581616916d33be1f
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-23 23:01:44 +0100

    Added todolist view.

--- lists/urls.py
+++ lists/urls.py
@@ -4,4 +4,5 @@ from lists import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    url(r'/todolist/^(?P<todolist_id>\d+)/', views.todolist, name='todolist')
 )
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from lists.models import TodoList
+
 
 def index(request):
     return HttpResponse('Hello, world.')
+
+
+def todolist(request, todolist_id):
+    todolist = get_object_or_404(TodoList, todolist_id)
+    return render(request, 'lists/todolist.html', {'todolist': todolist})
