f1bd9b33b0052d77c38fb614e5093987394e0aa4
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-24 21:37:29 +0100

    Fixed lists url config in root urls.py

--- todolist/urls.py
+++ todolist/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'todolist.views.home', name='home'),
-    url(r'^$', include('lists.urls')),
+    url(r'', include('lists.urls', namespace='lists')),
     url(r'^admin/', include(admin.site.urls)),
 )
