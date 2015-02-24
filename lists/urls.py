09d8fb2e5a151317731130322a962dd3793a8a7c
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-24 21:36:20 +0100

    Added new_todolist view to urls.

--- lists/urls.py
+++ lists/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 
 from lists import views
 
+
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'/todolist/^(?P<todolist_id>\d+)/', views.todolist, name='todolist')
+    url(r'^todolist/(?P<todolist_id>\d+)/', views.todolist, name='todolist'),
+    url(r'^todolist/new/', views.new_todolist, name='new_todolist'),
 )
