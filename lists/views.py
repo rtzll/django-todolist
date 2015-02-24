3793c62960a33b2e639e47f730b490ee0fae214e
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-24 21:36:43 +0100

    Added real index impl and new_todolist view.

--- lists/views.py
+++ lists/views.py
@@ -2,12 +2,20 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
 from lists.models import TodoList
+from lists.forms import TodoForm, TodoListForm
 
 
 def index(request):
-    return HttpResponse('Hello, world.')
+    return render(request, 'lists/index.html', {'form': TodoForm()})
 
 
 def todolist(request, todolist_id):
     todolist = get_object_or_404(TodoList, todolist_id)
     return render(request, 'lists/todolist.html', {'todolist': todolist})
+
+
+def new_todolist(request):
+    form = TodoListForm()
+    user = request.user.username if request.user.is_authenticated() else None
+    # ...
+    return render(request, 'lists/index.html', {'form': TodoForm()})
