cbfd4ea543760f1d8b26c354d3e5e8b087ade501
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-25 22:10:47 +0100

    Added todolist and accounts to installed apps.

--- todolist/settings.py
+++ todolist/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todolist',
     'lists',
+    'accounts',
 )
 
 MIDDLEWARE_CLASSES = (
