094c7123c5a1ee31bdf2a3ed1c6c13de7176c0ff
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-26 22:04:00 +0100

    Removed labels of account forms.

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,25 +2,27 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(max_length=64, widget=forms.TextInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
-    password = forms.CharField(max_length=64, widget=forms.PasswordInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
+    username = forms.CharField(label='', max_length=64,
+        widget=forms.TextInput(
+            attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
+    password = forms.CharField(label='', max_length=64,
+        widget=forms.PasswordInput(
+            attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
 
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(max_length=64, widget=forms.TextInput(
+    email = forms.EmailField(label='', max_length=64, widget=forms.TextInput(
         attrs={'class': 'u-full-width', 'placeholder': 'Email'}))
 
-    username = forms.CharField(max_length=64, widget=forms.TextInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
+    username = forms.CharField(label='', max_length=64,
+        widget=forms.TextInput(
+            attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
 
-    password = forms.CharField(max_length=64, widget=forms.PasswordInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
+    password = forms.CharField(label='', max_length=64,
+        widget=forms.PasswordInput(
+            attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
 
     password_confirmation = forms.CharField(
-        max_length=64, widget=forms.PasswordInput(
+        label='', max_length=64, widget=forms.PasswordInput(
             attrs={'class': 'u-full-width',
-                   'placeholder': 'Password confirmation'}
-        )
-    )
+                   'placeholder': 'Password confirmation'}))
