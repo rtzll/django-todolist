e4a9ab856c81a434e6eacab99d7c0612a1140269
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-26 22:04:58 +0100

    Appended list urls by $.

--- lists/urls.py
+++ lists/urls.py
@@ -5,6 +5,6 @@ from lists import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^todolist/(?P<todolist_id>\d+)/', views.todolist, name='todolist'),
-    url(r'^todolist/new/', views.new_todolist, name='new_todolist'),
+    url(r'^todolist/(?P<todolist_id>\d+)/$', views.todolist, name='todolist'),
+    url(r'^todolist/new/$', views.new_todolist, name='new_todolist'),
 )
