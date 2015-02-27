0d55d0d388b551b23a2f138b7980880c1dd3f4a9
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-27 23:26:53 +0100

    Refactored accounts forms.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,28 +1,27 @@
 from django import forms
 
 
+def widget_attrs(placeholder):
+    return {'class': 'u-full-width', 'placeholder': placeholder}
+
+
 class LoginForm(forms.Form):
     username = forms.CharField(label='', max_length=64,
-        widget=forms.TextInput(
-            attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
+        widget=forms.TextInput(attrs=widget_attrs('Username')))
     password = forms.CharField(label='', max_length=64,
-        widget=forms.PasswordInput(
-            attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
+        widget=forms.PasswordInput(attrs=widget_attrs('Password')))
 
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(label='', max_length=64, widget=forms.TextInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Email'}))
+    email = forms.EmailField(label='', max_length=64,
+        widget=forms.TextInput(attrs=widget_attrs('Email')))
 
     username = forms.CharField(label='', max_length=64,
-        widget=forms.TextInput(
-            attrs={'class': 'u-full-width', 'placeholder': 'Username'}))
+        widget=forms.TextInput(attrs=widget_attrs('Username')))
 
     password = forms.CharField(label='', max_length=64,
-        widget=forms.PasswordInput(
-            attrs={'class': 'u-full-width', 'placeholder': 'Password'}))
+        widget=forms.PasswordInput(attrs=widget_attrs('Password')))
 
-    password_confirmation = forms.CharField(
-        label='', max_length=64, widget=forms.PasswordInput(
-            attrs={'class': 'u-full-width',
-                   'placeholder': 'Password confirmation'}))
+    password_confirmation = forms.CharField(label='', max_length=64,
+        widget=forms.PasswordInput(attrs=widget_attrs('Password confirmation')
+    )
