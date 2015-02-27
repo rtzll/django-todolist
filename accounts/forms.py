09932fd8700fe72510f1a50f7c680f0d822e5c2a
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-27 23:29:25 +0100

    Style adjustments + closing bracket.

--- accounts/forms.py
+++ accounts/forms.py
@@ -15,13 +15,10 @@ class LoginForm(forms.Form):
 class RegistrationForm(forms.Form):
     email = forms.EmailField(label='', max_length=64,
         widget=forms.TextInput(attrs=widget_attrs('Email')))
-
     username = forms.CharField(label='', max_length=64,
         widget=forms.TextInput(attrs=widget_attrs('Username')))
-
     password = forms.CharField(label='', max_length=64,
         widget=forms.PasswordInput(attrs=widget_attrs('Password')))
-
     password_confirmation = forms.CharField(label='', max_length=64,
         widget=forms.PasswordInput(attrs=widget_attrs('Password confirmation')
-    )
+    ))
