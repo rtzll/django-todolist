96cae39a4224f93c4b212494b0471d77e054d250
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-27 23:40:02 +0100

    Added new views to urls.py

--- lists/urls.py
+++ lists/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^todolist/(?P<todolist_id>\d+)/$', views.todolist, name='todolist'),
     url(r'^todolist/new/$', views.new_todolist, name='new_todolist'),
+    url(r'^todolist/add/$', views.add_todolist, name='add_todolist'),
+    url(r'^todolists/$', views.overview, name='overview'),
 )
