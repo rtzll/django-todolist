f68eb3cbad8cdca1a20a00fdcd93c97705b1844c
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-27 23:07:01 +0100

    Extended lists views.

--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from django.http import HttpResponse
 
 from lists.models import TodoList
@@ -11,15 +11,33 @@ def index(request):
 
 def todolist(request, todolist_id):
     todolist = get_object_or_404(TodoList, todolist_id)
-    return render(request, 'lists/todolist.html', {'todolist': todolist})
+    return render(request, 'lists/todolist.html',
+                  {'todolist': todolist, 'form': TodoForm()})
 
 
 def overview(request):
     pass
 
 
+def stats(request):
+    return render(request, 'stats.html')
+
+
 def new_todolist(request):
-    form = TodoListForm()
-    user = request.user.username if request.user.is_authenticated() else None
-    # ...
+    if request.method == 'POST':
+        form = TodoListForm()
+        user = request.user.username if request.user.is_authenticated() else None
+        # ...
+        return redirect('todolist', todolist_id=1)
+
+    return render(request, 'lists/index.html', {'form': TodoForm()})
+
+
+def add_todolist(request):
+    if request.method == 'POST':
+        form = TodoListForm()
+        user = request.user.username if request.user.is_authenticated() else None
+        # ...
+        return redirect('todolist', todolist_id=1)
+
     return render(request, 'lists/index.html', {'form': TodoForm()})
