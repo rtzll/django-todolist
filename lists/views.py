517404b4bc086900b7d13a6f56d13b7b31e9c79d
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-28 23:07:46 +0100

    Implemented add/new todolist methods.

--- lists/views.py
+++ lists/views.py
@@ -19,27 +19,42 @@ def todolist(request, todolist_id):
 @login_required
 def overview(request):
     if request.method == 'POST':
-        todolist = None
-        # ...
-        return redirect('add_todolist', todolist)
+        form = TodoListForm(title=request.POST['title'])
+        if form.is_valid():
+            todolist = TodoList(title=request.POST['title'])
+            return redirect('add_todolist', todolist)
     return render(request, 'lists/overview.html', {'form': TodoListForm()})
 
 
 def new_todolist(request):
+
+    user = request.user if request.user.is_authenticated() else None
+
     if request.method == 'POST':
-        form = TodoListForm()
-        user = request.user.username if request.user.is_authenticated() else None
-        # ...
-        return redirect('todolist', todolist_id=1)
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            # create default todolist
+            todolist = TodoList(creator=user)
+            todolist.save()
+            todo = Todo(description=request.POST['description'],
+                        todolist_id=todolist.id, creator=user)
+            todo.save()
+            return redirect('todolist', todolist_id=todolist.id)
+        # else: show error message
 
     return render(request, 'lists/index.html', {'form': TodoForm()})
 
 
 def add_todolist(request):
+
+    user = request.user if request.user.is_authenticated() else None
+
     if request.method == 'POST':
-        form = TodoListForm()
-        user = request.user.username if request.user.is_authenticated() else None
-        # ...
-        return redirect('todolist', todolist_id=1)
+        form = TodoListForm(request.POST)
+        if form.is_valid():
+            todolist = TodoList(title=request.POST['title'], creator=user)
+            todolist.save()
+            return redirect('todolist', todolist_id=todolist.id)
+        # else: show error message
 
     return render(request, 'lists/index.html', {'form': TodoForm()})
