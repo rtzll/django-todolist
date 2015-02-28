1b21b0e723780cf9b86858ab118edf5a3f40a271
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-02-28 23:06:39 +0100

    Added count methods and default title to todolist.

--- lists/models.py
+++ lists/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import User
 
 
 class TodoList(models.Model):
-    title = models.CharField(max_length=128)
+    title = models.CharField(max_length=128, default='untitled')
     created_at = models.DateTimeField(auto_now=True)
     creator = models.ForeignKey(User)
 
     def __str__(self):
         return self.title
+
+    def count(self):
+        self.todo_set().count()
+
+    def count_finished(self):
+        self.todo_set().filter(is_finished=True).count()
+
+    def count_closed(self):
+        self.todo_set().count(is_finished=False).count()
 
 
 class Todo(models.Model):
