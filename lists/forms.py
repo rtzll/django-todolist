6eaef67dd51a32eecaf912d21b26ae91fb99d839
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-01 19:57:06 +0100

    Use same style for list forms.

--- lists/forms.py
+++ lists/forms.py
@@ -1,10 +1,15 @@
 from django import forms
 
 
+def widget_attrs(placeholder):
+    return {'class': 'u-full-width', 'placeholder': placeholder}
+
+
 class TodoForm(forms.Form):
-    description = forms.CharField(max_length=128, widget=forms.TextInput(
-        attrs={'class': 'u-full-width', 'placeholder': 'Enter your todo'}))
+    description = forms.CharField(max_length=128,
+        widget=forms.TextInput(attrs=widget_attrs('Enter your todo')))
 
 
 class TodoListForm(forms.Form):
-    title = forms.CharField(max_length=128)
+    title = forms.CharField(max_length=128, widget=forms.TextInput(
+        attrs=widget_attrs('Enter a title to start a new todolist')))
