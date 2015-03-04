e70ad3b4e5cc3617c96c5b15ad050dd416ffe5bf
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-04 23:06:26 +0100

    Removed router and added user API routes.

--- api/urls.py
+++ api/urls.py
@@ -1,16 +1,11 @@
 from django.conf.urls import url, include, patterns
-from rest_framework import routers
 
 from api import views
 
 
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-# router.register(r'^todolists/$', views.todolists)
-
-
 urlpatterns = patterns('',
-    url(r'^', include(router.urls)),
+    url(r'^users/$', views.UserList.as_view()),
+    url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^todolists/$', views.TodoLists.as_view()),
     url(r'^todolists/(?P<pk>[0-9]+)/$', views.TodoListDetail.as_view()),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
