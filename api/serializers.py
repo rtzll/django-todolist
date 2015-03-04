01d351cc0ff63e3b05ab6c62f5c57bf2f380605e
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-04 22:08:14 +0100

    Fixed typo.

--- api/serializers.py
+++ api/serializers.py
@@ -10,8 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('username', 'last_login', 'date_joined',
                   'email', 'todolist_set')
 
-
-class TodolistSerializer(serializers.ModelSerializer):
+class TodoListSerializer(serializers.ModelSerializer):
     class Meta:
         model = TodoList
         fields = ('id', 'title', 'created_at', 'creator')
