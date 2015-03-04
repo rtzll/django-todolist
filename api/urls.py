38aaac1b321c9a23808f812c246564c07d877584
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-04 22:07:12 +0100

    Added todolists to urls.

--- api/urls.py
+++ api/urls.py
@@ -6,9 +6,12 @@ from api import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+# router.register(r'^todolists/$', views.todolists)
 
 
 urlpatterns = patterns('',
     url(r'^', include(router.urls)),
+    url(r'^todolists/$', views.todolists),
+    url(r'^todolists/(?P<pk>[0-9]+)/$', views.todolist_detail),
     url(r'^api/', include('rest_framework.urls', namespace='rest_framework')),
 )
