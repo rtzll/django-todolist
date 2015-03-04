837a45c738c5751acb70710e3ed140fbfb4e3397
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-04 22:30:55 +0100

    Using generics.

--- api/views.py
+++ api/views.py
@@ -15,31 +15,12 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class TodoLists(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class TodoLists(generics.ListCreateAPIView):
+
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+class TodoListDetail(generics.RetrieveUpdateDestroyAPIView):
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class TodoListDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
