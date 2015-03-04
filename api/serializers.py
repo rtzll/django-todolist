37efcb57dbd396008c5e4624879ac31bb43adf17
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-04 23:07:25 +0100

    Added explicit todolists/creator.

--- api/serializers.py
+++ api/serializers.py
@@ -5,12 +5,20 @@ from lists.models import TodoList
 
 
 class UserSerializer(serializers.ModelSerializer):
+
+    todolists = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=TodoList.objects.all())
+
     class Meta:
         model = User
         fields = ('username', 'last_login', 'date_joined',
                   'email', 'todolists')
 
+
 class TodoListSerializer(serializers.ModelSerializer):
+
+    creator = serializers.ReadOnlyField(source='creator.username')
+
     class Meta:
         model = TodoList
         fields = ('id', 'title', 'created_at', 'creator')
