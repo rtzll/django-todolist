bdd029da0d52c8ca023d6d639566af38e0a524b3
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-06 23:44:52 +0100

    Outlined two basic test cases for API.

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class TodoListTests(APITestCase):
+
+    def test_get_todolist(self):
+        # add todolist
+        # get todolist
+        # check todolist
+        pass
+
+    def test_create_todolist(self):
+        url = reverse('api:todolist-list')
+        data = {'title': 'some title'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, data)
