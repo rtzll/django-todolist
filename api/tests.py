23cb83cfacb2d20ab539740186b0512d308ab298
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-07 20:35:07 +0100

    Added erroneous test cases for API.

--- api/tests.py
+++ api/tests.py
@@ -10,6 +10,7 @@ class TodoListTests(APITestCase):
     def setUp(self):
         User.objects.create_user('test', 'test@example.com', 'test')
         self.client.login(username='test', password='test')
+        self.test_data = {'title': 'some other title', 'todos': []}
 
     def tearDown(self):
         test_user = User.objects.get(username='test')
@@ -21,15 +22,13 @@ class TodoListTests(APITestCase):
         return self.client.post(url, data, format='json')
 
     def test_create_todolist(self):
-        data = {'title': 'some title', 'todos': []}
-        response = self.post_new_todolist(data)
+        response = self.post_new_todolist(self.test_data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(response.data['title'], data['title'])
+        self.assertEqual(response.data['title'], self.test_data['title'])
 
     def test_get_todolist(self):
-        data = {'title': 'some other title', 'todos': []}
         # add todolist
-        post_response = self.post_new_todolist(data)
+        post_response = self.post_new_todolist(self.test_data)
         self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
         # get todolist
         todolist_id = post_response.data['id']
@@ -39,3 +38,30 @@ class TodoListTests(APITestCase):
         self.assertEqual(get_response.status_code, status.HTTP_200_OK)
         # check todolist
         self.assertEqual(get_response.data, post_response.data)
+
+    def test_get_when_not_logged_in(self):
+        # add some data
+        post_response = self.post_new_todolist(self.test_data)
+        self.assertEqual(post_response.status_code, status.HTTP_201_CREATED)
+        # make sure the user is logged out
+        self.client.logout()
+        response = self.client.get('/api/todolists/1/')
+        # expect 200, because reading is allowed for everybody
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_non_existent_todolist(self):
+        response = self.client.get('/api/todolists/0/')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+    def test_post_when_not_logged_in(self):
+        # make sure the user is logged out
+        self.client.logout()
+        # try posting a todolist
+        response = self.post_new_todolist(self.test_data)
+
+        self.assertEqual(
+            response.data['detail'],
+            'Authentication credentials were not provided.'
+        )
+
