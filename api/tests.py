e15ca6da5f5e56ce44290b3e904e9615329e39b5
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-09 21:43:41 +0100

    Added API user test cases.

--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,29 @@ from rest_framework.test import APITestCase
 
 from lists.models import TodoList
 
+class UserTests(APITestCase):
+
+    def setUp(self):
+        User.objects.create_user('test', 'test@example.com', 'test')
+        self.client.login(username='test', password='test')
+
+    def tearDown(self):
+        User.objects.get(username='test').delete()
+        self.client.logout()
+
+    def test_post_on_read_only(self):
+        response = self.client.post('api:user-list', {}, format='json')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_user(self):
+        # get user (test user from setup)
+        get_response = self.client.get(
+            '/api/users/{0}/'.format(1)
+        )
+        self.assertEqual(get_response.status_code, status.HTTP_200_OK)
+        # check user
+        self.assertEqual(get_response.data['username'], 'test')
+
 
 class TodoListTests(APITestCase):
 
