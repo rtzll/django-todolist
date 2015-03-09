340b39fb0b25ad11b5ccde49217688a08adf8aff
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-09 21:43:12 +0100

    Added accounts test cases.

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import User
 
-# Create your tests here.
+from accounts.forms import RegistrationForm, LoginForm
+
+class AccountsTests(TestCase):
+
+    def setUp(self):
+        User.objects.create_user('test', 'test@example.com', 'test')
+
+    def tearDown(self):
+        User.objects.get(username='test').delete()
+
+    def test_get_register(self):
+        response = self.client.get(reverse('auth:register'))
+        self.assertTemplateUsed(response, 'accounts/register.html')
+        self.assertIsInstance(response.context['form'], RegistrationForm)
+
+    def test_get_login(self):
+        response = self.client.get(reverse('auth:login'))
+        self.assertTemplateUsed(response, 'accounts/login.html')
+        self.assertIsInstance(response.context['form'], LoginForm)
+
+    def test_register(self):
+        register_data = {
+            'email': 'new@user.com',
+            'username': 'new_user',
+            'password': 'test',
+            'password_confirmation': 'test'
+        }
+        response = self.client.post(
+            reverse('auth:register'), data=register_data
+        )
+        self.assertEqual(response.status_code, 302)
+        # new user was created
+        self.assertIsNotNone(User.objects.get(username='new_user'))
+
+    def test_login(self):
+        # no user is logged in
+        self.assertFalse('_auth_user_id' in self.client.session)
+        login_data = {'username': 'test', 'password': 'test'}
+        response = self.client.post(reverse('auth:login'), data=login_data)
+        self.assertEqual(response.status_code, 302)
+        # user is logged in
+        self.assertEqual(self.client.session['_auth_user_id'], 1)
+
+    # TODO add erroneous test cases for login and register (invalid data)
+
+    def test_logout(self):
+        response = self.client.get(reverse('auth:logout'))
+        self.assertEqual(response.status_code, 302)
+        # no user logged in anymore
+        self.assertFalse('_auth_user_id' in self.client.session)
