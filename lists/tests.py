b0bdac2124e0fd862077e34820ed3af9a1daf275
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-11 22:28:21 +0100

    Extended list test cases.

--- lists/tests.py
+++ lists/tests.py
@@ -1,36 +1,80 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
 
 from lists.forms import TodoForm, TodoListForm
+from lists.models import Todo, TodoList
 
 
 class ListTests(TestCase):
 
     def setUp(self):
-        User.objects.create_user('test', 'test@example.com', 'test')
+        self.user = User.objects.create_user(
+            'test', 'test@example.com', 'test'
+        )
+        self.todolist = TodoList(title='test title', creator=self.user)
+        self.todolist.save()
+        self.todo = Todo(
+            description='save todo',
+            todolist_id=self.todolist.id,
+            creator=self.user
+        )
+        self.todo.save()
         self.client.login(username='test', password='test')
 
     def tearDown(self):
-        User.objects.get(username='test').delete()
         self.client.logout()
+        self.user.delete()
+        self.todolist.delete()
+        self.todo.delete()
 
     def test_get_index_page(self):
-        pass
+        response = self.client.get(reverse('lists:index'))
+        self.assertTemplateUsed(response, 'lists/index.html')
+        self.assertIsInstance(response.context['form'], TodoForm)
 
     def test_add_todo_to_index_page(self):
-        pass
+        response = self.client.post(
+            reverse('lists:index'), {'description': 'test'}
+        )
+        self.assertTemplateUsed(response, 'lists/index.html')
+        self.assertIsInstance(response.context['form'], TodoForm)
 
     def test_get_todolist_view(self):
-        pass
+        response = self.client.get(
+            reverse(
+                'lists:todolist', kwargs={'todolist_id': self.todolist.id}
+            )
+        )
+        self.assertTemplateUsed(response, 'lists/todolist.html')
+        self.assertIsInstance(response.context['form'], TodoForm)
 
     def test_add_todo_to_todolist_view(self):
-        pass
+        response = self.client.post(
+            reverse(
+                'lists:todolist', kwargs={'todolist_id': self.todolist.id}
+            ),
+            {'description': 'test'}
+        )
+        self.assertTemplateUsed(response, 'lists/todolist.html')
+        self.assertIsInstance(response.context['form'], TodoForm)
 
     def test_get_todolist_overview(self):
-        pass
+        response = self.client.get(reverse('lists:overview'))
+        self.assertTemplateUsed(response, 'lists/overview.html')
+        self.assertIsInstance(response.context['form'], TodoListForm)
+
+    def test_get_todolist_overview_redirect_when_not_logged_in(self):
+        self.client.logout()
+        response = self.client.get(reverse('lists:overview'))
+        self.assertEqual(response.status_code, 302)
 
     def test_add_todolist_to_todolist_overview(self):
-        pass
+        response = self.client.post(
+            reverse('lists:overview'), {'title': 'some title'}
+        )
+        self.assertTemplateUsed(response, 'lists/overview.html')
+        self.assertIsInstance(response.context['form'], TodoListForm)
 
 
 class TodoListFormTests(TestCase):
