f7168fada43c4d8703860f72fdad9d327e203b1e
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-12 22:10:54 +0100

    Fixed putNewStatus to use csrf token.

--- todolist/static/js/site.js
+++ todolist/static/js/site.js
@@ -80,8 +80,24 @@ $(document).ready(function() {
     }
   }
 
+  function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+  }
+
   function putNewStatus(todoID, isFinished) {
-    todoURL = '/api/todos/' + todoID + '/'
+
+    // setup ajax to csrf token
+    var csrftoken = getCookie('csrftoken');
+    $.ajaxSetup({
+      beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+          xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+      }
+    });
+    // send put request using the get data
+    var todoURL = '/api/todos/' + todoID + '/'
     $.getJSON(todoURL, function(data) {
       data.is_finished = isFinished;
       if (isFinished) {
@@ -92,12 +108,29 @@ $(document).ready(function() {
         url: todoURL,
         type: 'PUT',
         contentType: 'application/json',
-        data: {'id': '12'}, // data,
+        data: data,
         success: function() {
           location.reload();
         }
       });
     });
+  }
+
+  // function form the django docs
+  function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
   }
 
   init();
