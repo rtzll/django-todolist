c52d501c9279ffe0f4a131d333c1314547575d9a
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-06 22:25:01 +0200

    Refactored auth form creation.

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,29 +5,49 @@ def widget_attrs(placeholder):
     return {'class': 'u-full-width', 'placeholder': placeholder}
 
 
+def forms_kwargs(widget, label='', max_length=64):
+    return {'widget': widget, 'label': label, 'max_length': max_length}
+
+
 class LoginForm(forms.Form):
-    username = forms.CharField(label='', max_length=64,
-        widget=forms.TextInput(attrs=widget_attrs('Username')))
-    password = forms.CharField(label='', max_length=64,
-        widget=forms.PasswordInput(attrs=widget_attrs('Password')))
+
+    username = forms.CharField(
+        forms_kwargs(widget=forms.TextInput(attrs=widget_attrs('Username')))
+    )
+    password = forms.CharField(
+        forms_kwargs(
+            widget=forms.PasswordInput(attrs=widget_attrs('Password'))
+        )
+    )
 
 
 class RegistrationForm(forms.Form):
-    email = forms.EmailField(label='', max_length=64,
-        widget=forms.TextInput(attrs=widget_attrs('Email')))
-    username = forms.CharField(label='', max_length=64,
-        widget=forms.TextInput(attrs=widget_attrs('Username')))
-    password = forms.CharField(label='', max_length=64,
-        widget=forms.PasswordInput(attrs=widget_attrs('Password')))
-    password_confirmation = forms.CharField(label='', max_length=64,
-        widget=forms.PasswordInput(attrs=widget_attrs('Password confirmation')
-    ))
+
+    email = forms.EmailField(
+        forms_kwargs(widget=forms.TextInput(attrs=widget_attrs('Email')))
+    )
+    username = forms.CharField(
+        forms_kwargs(widget=forms.TextInput(attrs=widget_attrs('Username')))
+    )
+
+    password = forms.CharField(
+        forms_kwargs(
+            widget=forms.PasswordInput(attrs=widget_attrs('Password'))
+        )
+    )
+    password_confirmation = forms.CharField(
+        forms_kwargs(
+            widget=forms.PasswordInput(
+                attrs=widget_attrs('Password confirmation')
+            )
+        )
+    )
 
     def clean(self):
         password = self.cleaned_data.get('password')
         password_confirmation = self.cleaned_data.get('password_confirmation')
 
         if password and password != password_confirmation:
-            raise forms.ValidationError("Passwords don't match")
+            raise forms.ValidationError("Passwords don't match.")
 
         return self.cleaned_data
