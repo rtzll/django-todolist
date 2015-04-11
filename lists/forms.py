fe4e96d22cda21a7a1b6509e75498c7e15e41f79
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-11 23:52:46 +0200

    Added a default empty label for list forms.

--- lists/forms.py
+++ lists/forms.py
@@ -5,8 +5,8 @@ def widget_attrs(placeholder):
     return {'class': 'u-full-width', 'placeholder': placeholder}
 
 
-def form_kwargs(widget, max_length=128):
-    return {'widget': widget, 'max_length': max_length}
+def form_kwargs(widget, label='', max_length=128):
+    return {'widget': widget, 'label': label, 'max_length': max_length}
 
 
 class TodoForm(forms.Form):
