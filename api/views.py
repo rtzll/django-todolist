a5e3dfe7a94e8c5998fc9998dc546d8a9418282a
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-04-13 21:46:00 +0200

    Customized permissions.

--- api/views.py
+++ api/views.py
@@ -6,6 +6,26 @@ from api.serializers import UserSerializer, TodoListSerializer, TodoSerializer
 from lists.models import TodoList, Todo
 
 
+class IsCreatorOrReadOnly(permissions.BasePermission):
+    """
+    Object-level permission to only allow owners of an object to edit it.
+    Assumes the model instance has an `creator` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # If the object doesn't have a creator (i.e. anon) allow all methods.
+        if not obj.creator:
+            return True
+
+        # Instance must have an attribute named `creator`.
+        return obj.creator == request.user
+
+
 class UserViewSet(viewsets.ModelViewSet):
 
     queryset = User.objects.all()
@@ -17,7 +37,7 @@ class TodoListViewSet(viewsets.ModelViewSet):
 
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsCreatorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
@@ -27,7 +47,7 @@ class TodoViewSet(viewsets.ModelViewSet):
 
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    permission_classes = (IsCreatorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(creator=self.request.user)
