664137ae9072cd3de00b9e5856ac7f021dee5be0
AlejandrVilla <avillah@unsa.edu.pe>
2022-12-28 04:56:27 -0500

    pruebas funcionales

--- functional.py
+++ functional.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 import time
 
 
