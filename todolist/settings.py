7295e89124188723ebfa0a0c84d349e814620bd7
Tishaun Bristol <Tishaunbristol@me.com>
2023-10-29 01:16:47 -0400

    testing

--- todolist/settings.py
+++ todolist/settings.py
@@ -18,6 +18,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# ADDED TO GIT HUB 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
